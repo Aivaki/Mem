57612b8c9788263988f3309a4246b29461f525ff
Aivaki <58384167+Aivaki@users.noreply.github.com>
2022-05-28 14:26:52 +0500

    Update engine.hpp

--- cheat/engine.hpp
+++ cheat/engine.hpp
@@ -24,15 +24,15 @@ namespace engine
 	public:
 
 		/* Engine functions */
-		DWORD getEnginePointer(const CSGOMemory& mem) const noexcept(false) {
+		DWORD getEnginePointer(const CSGOMemory& mem) const noexcept(true) {
 			return *mem.Read<DWORD>(mem.getEngineBase() + Updater.dwClientState);
 		}
 
-		[[nodiscard]] void forceUpdate(const CSGOMemory& mem) noexcept(false) {
+		[[nodiscard]] void forceUpdate(const CSGOMemory& mem) noexcept(true) {
 			mem.Write<int>(getEnginePointer(mem) + 0x16C, -1);
 		}
 		
-		[[nodiscard]] bool isInGame(const CSGOMemory& mem) const noexcept(false) {
+		[[nodiscard]] bool isInGame(const CSGOMemory& mem) const noexcept(true) {
 			if (mem.Read<int>(getEnginePointer(mem) + Updater.dwClientState_State) == 6) {
 				return true;
 			}
